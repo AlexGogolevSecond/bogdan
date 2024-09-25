f3fde136041a95796bcf236ede9479d685953077
Aleksandr <a.gogolev@carbonsoft.ru>
2024-09-25 10:29:02 +0500

    123

---
+++ theory/destruct_func.js
@@ -0,0 +1,19 @@
+const userProfile = {
+    name: 'Alex',
+    commentsQnt: 25,
+    hasLalala: true,
+}
+
+const userInfo = (profile) => {
+    let name, commentsQnt, hasLalala = profile
+    // console.log(`name: ${name}`);
+    // console.log(`commentsQnt: ${commentsQnt}`);
+    // console.log(`hasLalala: ${hasLalala}`);
+    if (!commentsQnt) {
+        return 
+    }
+
+}
+
+userInfo(userProfile)
+
